d370201a880676ec9ece2caf470f1ee0c7d6fe93
Lucas Vance <lucas.vance@protonmail.com>
2022-04-06 17:09:29 +0000

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,14 @@
+import math
+
+# Get two sets of coordinates from user
+x1 = int(input("x1: "))
+y1 = int(input("y1: "))
+
+x2 = int(input("x2: "))
+y2 = int(input("y2: "))
+
+# Find distance using the distance formula
+distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+# Print distance and round with two digits
+print("Distance: " + round(distance, 2))
