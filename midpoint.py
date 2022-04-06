61389429d97f5f6895cbc487f5409b7b7ca6f022
Lucas Vance <lucas.vance@protonmail.com>
2022-04-06 16:23:19 +0000

    Update midpoint.py

---
+++ midpoint.py
@@ -0,0 +1,14 @@
+import math
+
+# Get two coordinates from user
+x1 = input("x1: ")
+y1 = input("y1: ")
+
+x2 = input("x2: ")
+y2 = input("y2: ")
+
+
+xCoor = (int(x1) + int(x2))/2
+yCoor = (int(y1) + int(y2))/2
+
+print("Coordinates of midpoint:  " + str(xCoor) + ", " + str(yCoor))
