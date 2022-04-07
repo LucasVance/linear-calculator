550c96a99e005b390d04df4b8cff40692da0cc0f
Lucas Vance <lucas.vance@protonmail.com>
2022-04-07 15:27:21 +0000

    Loop main.py to ensure continuous use

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-toRun = input("Property to find: ")
+while (True):
+    toRun = input("Property to find: ")
 
-if toRun == "distance":
-    exec(open("./distance.py").read())
-elif toRun == "slope":
-    exec(open("./slope.py").read())
-elif toRun == "midpoint":
-    exec(open("./midpoint.py").read())
-else:
-    print("An error occurred.")
+    if toRun == "distance":
+        exec(open("./distance.py").read())
+    elif toRun == "slope":
+        exec(open("./slope.py").read())
+    elif toRun == "midpoint":
+        exec(open("./midpoint.py").read())
+    else:
+        print("An error occurred. Please enter one of the following: distance, midpoint, slope.")
